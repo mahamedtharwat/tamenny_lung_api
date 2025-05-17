24ea9b5cac3964ebecb50aff7fecb95105452ac7
mahamedtharwat <mohamedmorsy8798@gmail.com>
2025-05-17 22:50:06 +0300

    edit the way of input entering

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
 from flask import Flask, request, jsonify
 import numpy as np
 from tensorflow.keras.models import load_model
+from PIL import Image
+import io
 
 app = Flask(__name__)
 
-# Load the model
 model = load_model('model.h5')
-
-# Class labels in the same order the model was trained with
 class_labels = ['Benign', 'Malignant', 'Normal']
-
 
 @app.route('/')
 def home():
     return "Lung Prediction Model is live!"
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided'}), 400
 
-    # Make sure input is a list of numbers
-    features = np.array(data['features']).reshape(1, -1)
+    image_file = request.files['image']
+    image = Image.open(image_file).convert('RGB')
+    
+    # Resize based on model input shape (for example 224x224)
+    image = image.resize((224, 224))
+    image_array = np.array(image) / 255.0
+    image_array = image_array.reshape(1, 224, 224, 3)
 
-    prediction = model.predict(features)  # Output: probabilities
+    prediction = model.predict(image_array)
     predicted_index = int(np.argmax(prediction))
     predicted_label = class_labels[predicted_index]
 
     return jsonify({'result': predicted_label})
-
 
 if __name__ == '__main__':
     import os
